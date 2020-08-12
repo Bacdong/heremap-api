e2dba128d680dcd042469e42b08eaea014ab2d07
Bacdong <duongdong2203@gmai.com>
2020-08-12 17:33:58 +0700

    Change field type serializer for lat, lng

--- address/serializer.py
+++ address/serializer.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 
 class ListAddressSubjectionSerializer(serializers.Serializer):
+
+    keyword = serializers.CharField(max_length = 40)
     # number = serializers.CharField(max_length = 40)
     # street = serializers.CharField(max_length = 40)
     # ward = serializers.CharField(max_length = 40)
     # district = serializers.CharField(max_length = 40)
-    keyword = serializers.CharField(max_length = 40)
 
 
 class AddressSerializer(serializers.Serializer):
@@ -13,8 +14,10 @@ class AddressSerializer(serializers.Serializer):
 
 
 class GeoSerializer(serializers.Serializer):
-    lat = serializers.CharField(max_length = 40)
-    lng = serializers.CharField(max_length = 40)
+    lat = serializers.FloatField()
+    lng = serializers.FloatField()
+    # lat = serializers.CharField(max_length = 40)
+    # lng = serializers.CharField(max_length = 40)
 
 class LocationIdSerializer(serializers.Serializer):
     location_id = serializers.CharField(max_length = 100)
